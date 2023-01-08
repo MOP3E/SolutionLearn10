a4918ed365d7b83bca2ea2b82507f51e8365112c
MOP3E <MOP3E@DESKTOP-I2EHRHL>
2023-01-08 21:44:20 +0300

    Добавьте файлы проекта.

---
+++ ArcanoidSfml/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SFML.Window;
+using SFML.Graphics;
+using SFML.Audio;
+using SFML.System;
+
+namespace ArcanoidSfml
+{
+    internal class Program
+    {
+        private static RenderWindow _window;
+
+        static void Main(string[] args)
+        {
+            _window = new RenderWindow(new VideoMode(800, 600), "Арканоид");
+            _window.SetFramerateLimit(60);
+            _window.Closed += _window_Closed;
+
+            while(_window.IsOpen)
+            {
+                _window.Clear(Color.Cyan);
+
+                _window.DispatchEvents();
+
+                _window.Display();
+            }
+        }
+
+        private static void _window_Closed(object? sender, EventArgs e)
+        {
+            _window.Close();
+        }
+    }
+}
